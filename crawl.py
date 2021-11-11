48d773d734116fad8404ef5c6c9f5002d30b597a
Kamyar Gerami <kamyargeramiasl@gmail.com>
2021-11-11 23:49:43 +0330

    change filename

---
+++ crawl.py
@@ -0,0 +1,31 @@
+import sys
+from datetime import datetime
+from api_providers.alphavantage import alphavantage
+from api_providers.twelvedata import twelvedata
+from telegram import telegram
+
+provider = sys.argv[1] if len(sys.argv[1:]) else 'twelvedata'
+
+print('--------------------------------')
+print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), '-', provider)
+results = twelvedata().result() if provider == 'twelvedata' else alphavantage().result()
+print('--------------------------------')
+
+if not len(results):
+    exit()
+
+text = '📊 RSI Choices in Daily Rates: \n\n'
+buttons = []
+for result in results:
+    buttons.append([{
+        'text': '%s (%s)' % (result['symbol'], result['hour_rate']),
+        'url': 'https://www.tradingview.com/chart?symbol=FX%3A' + result['symbol'].replace('/', '')
+    }])
+
+    text += '%s (%s) - %s \n ------------ \n' % (result['symbol'], result['daily_rate'], ('Buy ⬆️' if result['daily_rate'] < 35 else 'Sell ⬇️'))
+text += '\n Their Hourly Rates:'
+
+telegram.send(text, buttons, {
+    # 'http': 'http://127.0.0.1:1080',
+    # 'https': 'http://127.0.0.1:1080',
+})
