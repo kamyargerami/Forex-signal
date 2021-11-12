412f0bf21fd93448e386ffa9bb6cbe48ab4ef51a
Kamyar Gerami <kamyargeramiasl@gmail.com>
2021-11-12 22:28:36 +0330

    remove   coke from twlvedata

--- api_providers/twelvedata.py
+++ api_providers/twelvedata.py
@@ -3,8 +3,8 @@ from api_providers.provider import provider
 from datetime import datetime
 
 class twelvedata(provider):
-    ## 33 symbols support per hour - 25 added
-    symbols = ['EUR/USD', 'USD/JPY', 'GBP/USD', 'AUD/USD', 'USD/CAD', 'USD/CHF', 'NZD/USD', 'EUR/GBP', 'EUR/AUD', 'EUR/CHF', 'EUR/JPY', 'EUR/NZD', 'GBP/EUR', 'GBP/JPY', 'GBP/AUD', 'GBP/CAD', 'GBP/CHF', 'GBP/NZD', 'FB', 'IBM', 'AAPL', 'PEP', 'COKE', 'BABA', 'MSFT']
+    ## 33 symbols support per hour - 24 added
+    symbols = ['EUR/USD', 'USD/JPY', 'GBP/USD', 'AUD/USD', 'USD/CAD', 'USD/CHF', 'NZD/USD', 'EUR/GBP', 'EUR/AUD', 'EUR/CHF', 'EUR/JPY', 'EUR/NZD', 'GBP/EUR', 'GBP/JPY', 'GBP/AUD', 'GBP/CAD', 'GBP/CHF', 'GBP/NZD', 'FB', 'IBM', 'AAPL', 'PEP', 'BABA', 'MSFT']
 
     def get_hourly_rsi_rate(self, symbol):
         response = requests.get('https://api.twelvedata.com/rsi?symbol=%s&interval=1h&apikey=f4ca8bae96fe4fd88717d7bdbbba1d37' % symbol)
